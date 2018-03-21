394d6964d96ee139f9676dd4cd39cca3f966fd88
atulsomark <achaudhary@somarkinnovations.com>
2018-03-21 15:06:07 +1030

    got integration test working

---
+++ src/test/java/io/javabrains/sbs/topic/TopicRespositoryTest.java
@@ -0,0 +1,31 @@
+package io.javabrains.sbs.topic;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopicRespositoryTest {
+
+	@Autowired
+	private TopicRespository topicRespository;
+
+	@Test
+	public void test() {
+		assertNotNull(topicRespository);
+
+		topicRespository.save(new Topic("desc", "name"));
+
+		Topic tp = topicRespository.findByName("name");
+
+		System.out.println(tp.getDescription() + " " + tp.getId());
+
+		assertNotNull(tp);
+	}
+
+}
